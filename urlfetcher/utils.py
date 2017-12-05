fd90c31f4fe0ee7268cf025eaf34ffa8693f5ac7
Lars Peter Søndergaard <nitori@chireiden.net>
2017-12-05 15:32:43 +0100

    Improve user friendliness (2)

--- urlfetcher/utils.py
+++ urlfetcher/utils.py
@@ -1,11 +1,20 @@
 import math
+from datetime import datetime
+import pytz
 
 
 def format_size(size):
-    prefixes = ['', '*2^10', '*2^20', '*2^30', '*2^40', '*2^50', '*2^60']
+    tz = pytz.timezone('Europe/Berlin')
+    dt = datetime.now(tz)
+    plus = ''
+    if dt.hour == 13 and dt.minute == 37:
+        plus = ' Bytes'
+        prefixes = ['', '*2^10', '*2^20', '*2^30', '*2^40', '*2^50', '*2^60']
+    else:
+        prefixes = [' B', ' KB', ' MB', ' GB', ' TB']
     exp = int(math.log(size, 1024))
     exp = min(exp, len(prefixes) - 1)
-    return '{:.1f}{} Bytes'.format(size/(1024**exp), prefixes[exp])
+    return '{:.1f}{}{}'.format(size/(1024**exp), prefixes[exp], plus)
 
 
 def format_duration(seconds):
